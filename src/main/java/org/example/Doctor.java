fd5e17b64654810c6d9792433cc8340e2c6c6c73
6298805 <jccpainchaud@gmail.com>
2025-04-29 14:01:02 -0400

    Created Doctor class, implemented skeleton

---
+++ src/main/java/org/example/Doctor.java
@@ -0,0 +1,82 @@
+package org.example;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Doctor extends User {
+    private List<Appointment> appointments = new ArrayList<>();
+
+    public Doctor(String name, Sex sex) {
+        super(name, sex);
+    }
+
+    public void viewAppointments() {
+        for (Appointment appointment : appointments) {
+            Patient patient = appointment.getPatient();
+            LocalDateTime time = appointment.getTime();
+            boolean confirmed = appointment.isConfirmed();
+
+            System.out.println("Patient: " + patient
+                    + "\nTime: " + time
+                    + "\nConfirmed?: " + confirmed
+                    + "\n");
+        }
+    }
+
+    public void viewPrescriptions(Patient patient) {
+        patient.viewPrescriptions();
+    }
+
+    public void viewMedicalRecords(Patient patient) {
+        patient.viewMedicalRecords();
+    }
+
+    public void provideConsultation(Patient patient) {
+        // TODO: Implement
+    }
+
+    public void providePrescription(Patient patient, Prescription prescription) {
+        patient.getPrescriptions().add(prescription);
+    }
+
+    public void updateMedicalRecords(Patient patient, MedicalRecord medicalRecord) {
+        patient.setMedicalRecord(medicalRecord);
+    }
+
+    @Override
+    public void displayDashboard() {
+        // TODO: Implement
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Doctor doctor = (Doctor) o;
+        return Objects.equals(appointments, doctor.appointments);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), appointments);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " Doctor{" +
+                "appointments=" + appointments +
+                '}';
+    }
+
+    public List<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(List<Appointment> appointments) {
+        this.appointments = appointments;
+    }
+}
