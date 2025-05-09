a5771f1138d5eca8b5e98e65c707f89658f0bd56
waffleprinter <dwamsie@gmail.com>
2025-05-09 00:46:42 -0400

    added equals, tostring, getters and setters

--- src/main/java/org/example/LabTest.java
+++ src/main/java/org/example/LabTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class LabTest extends MedicalOrder {
     private String labName;
@@ -13,5 +14,59 @@ public class LabTest extends MedicalOrder {
         this.labName = labName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        LabTest labTest = (LabTest) o;
+        return Objects.equals(labName, labTest.labName) && Objects.equals(collectionDate, labTest.collectionDate) && Objects.equals(resultDate, labTest.resultDate) && Objects.equals(summary, labTest.summary);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), labName, collectionDate, resultDate, summary);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " LabTest{" +
+                "labName='" + labName + '\'' +
+                ", collectionDate=" + collectionDate +
+                ", resultDate=" + resultDate +
+                ", summary='" + summary + '\'' +
+                '}';
+    }
+
+    public String getLabName() {
+        return labName;
+    }
+
+    public void setLabName(String labName) {
+        this.labName = labName;
+    }
+
+    public LocalDateTime getCollectionDate() {
+        return collectionDate;
+    }
+
+    public void setCollectionDate(LocalDateTime collectionDate) {
+        this.collectionDate = collectionDate;
+    }
+
+    public LocalDateTime getResultDate() {
+        return resultDate;
+    }
+
+    public void setResultDate(LocalDateTime resultDate) {
+        this.resultDate = resultDate;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
 }
