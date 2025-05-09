7c3bed7053812879d13c29b143d5fa7d0e48a46c
waffleprinter <dwamsie@gmail.com>
2025-05-09 17:40:37 -0400

    deleted

--- src/main/java/org/example/RecordAccesible.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public interface RecordAccesible {
-    void viewMedicalRecords(Patient patient);
-    void updateMedicalRecords(Patient patient, MedicalRecord medicalRecord);
-}
