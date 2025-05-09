6f70ae29b1177fd5fabd4a0864467061d805abb4
waffleprinter <dwamsie@gmail.com>
2025-05-09 00:39:17 -0400

    added equals, tostring, getters and setters

--- src/main/java/org/example/Prescription.java
+++ src/main/java/org/example/Prescription.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Prescription extends MedicalOrder {
     private String medication;
@@ -13,6 +14,62 @@ public class Prescription extends MedicalOrder {
         this.medication = medication;
         this.dosage = dosage;
         this.frequency = frequency;
+        this.expiryDate = expiryDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Prescription that = (Prescription) o;
+        return Objects.equals(medication, that.medication) && Objects.equals(dosage, that.dosage) && Objects.equals(frequency, that.frequency) && Objects.equals(expiryDate, that.expiryDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), medication, dosage, frequency, expiryDate);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " Prescription{" +
+                "medication='" + medication + '\'' +
+                ", dosage='" + dosage + '\'' +
+                ", frequency='" + frequency + '\'' +
+                ", expiryDate=" + expiryDate +
+                '}';
+    }
+
+    public String getMedication() {
+        return medication;
+    }
+
+    public void setMedication(String medication) {
+        this.medication = medication;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public LocalDateTime getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDateTime expiryDate) {
         this.expiryDate = expiryDate;
     }
 }
