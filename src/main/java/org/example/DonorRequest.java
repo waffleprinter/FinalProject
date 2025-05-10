63260ee4b6f768252a4dcd669f32b1408eb517ba
waffleprinter <dwamsie@gmail.com>
2025-05-09 20:40:26 -0400

    added donor variable and adjusted generated methods accordingly

--- src/main/java/org/example/DonorRequest.java
+++ src/main/java/org/example/DonorRequest.java
@@ -8,16 +8,19 @@ public class DonorRequest extends MedicalOrder {
     private BloodType bloodType;
     private LocalDateTime matchDate;
     private LocalDateTime fulfilledDate;
+    private Donor donor;
 
     public DonorRequest(Doctor doctor, Patient patient, DonorType donorType) {
-        super(doctor, patient);
-        this.donorType = donorType;
+        this(doctor, patient, donorType, null);
     }
 
     public DonorRequest(Doctor doctor, Patient patient, DonorType donorType, BloodType bloodType) {
         super(doctor, patient);
         this.donorType = donorType;
         this.bloodType = bloodType;
+        this.matchDate = null;
+        this.fulfilledDate = null;
+        this.donor = null;
     }
 
     /**
@@ -28,7 +31,8 @@ public class DonorRequest extends MedicalOrder {
         System.out.println("Donor Type: " + donorType
         + "\nBlood Type: " + bloodType
         + "\nMatch Date: " + matchDate
-        + "\nFulfilled Date: " + fulfilledDate);
+        + "\nFulfilled Date: " + fulfilledDate
+        + "\nDonor: " + donor);
     }
 
     @Override
@@ -36,12 +40,12 @@ public class DonorRequest extends MedicalOrder {
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         DonorRequest that = (DonorRequest) o;
-        return donorType == that.donorType && bloodType == that.bloodType && Objects.equals(matchDate, that.matchDate) && Objects.equals(fulfilledDate, that.fulfilledDate);
+        return donorType == that.donorType && bloodType == that.bloodType && Objects.equals(matchDate, that.matchDate) && Objects.equals(fulfilledDate, that.fulfilledDate) && Objects.equals(donor, that.donor);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), donorType, bloodType, matchDate, fulfilledDate);
+        return Objects.hash(super.hashCode(), donorType, bloodType, matchDate, fulfilledDate, donor);
     }
 
     @Override
@@ -52,6 +56,7 @@ public class DonorRequest extends MedicalOrder {
                 ", bloodType=" + bloodType +
                 ", matchDate=" + matchDate +
                 ", fulfilledDate=" + fulfilledDate +
+                ", donor=" + donor +
                 '}';
     }
 
@@ -85,5 +90,13 @@ public class DonorRequest extends MedicalOrder {
 
     public void setFulfilledDate(LocalDateTime fulfilledDate) {
         this.fulfilledDate = fulfilledDate;
+    }
+
+    public Donor getDonor() {
+        return donor;
+    }
+
+    public void setDonor(Donor donor) {
+        this.donor = donor;
     }
 }
